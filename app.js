996ec80865b6c4ec48ef23e1cad76ba3ed68fb62
obada <obadaquraan@yahoo.com>
2020-01-01 21:42:13 +0200

    app.js add http(s)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $('textarea').hide();
 let id = $('#trailer').data('id');
 console.log("ID", id);
 
-$.get(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=57a6b853590432570e83f7520825c046`).then(data=>{
+$.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=57a6b853590432570e83f7520825c046`).then(data=>{
     $("#trailer").attr('src',`https://www.youtube.com/embed/${data.results[0].key}`);
 });
 
